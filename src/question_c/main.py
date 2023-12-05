8f3111c191e45cbc25c5ea41b55a83bf70516e7d
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-12-05 04:54:09 +0000

    question c

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,19 @@
 #add import
+import question_c
+
+while True:
+    print("\nmenu")
+    print("option 1 - display stock history")
+    print("option 2 - exit menu")
+
+    option =  str(input ("Pick a number: "))
+
+    while option not in ("1" , "2"):
+        option = str(input("ERROR pick 1 or 2: "))
+
+    if option == "1":
+        question_c.stock_purchase_history()
+
+    if option == "2":
+         print ("exiting program")
+         break
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,34 @@
 #write functions here, don't add input('') statements here!
+
+class stock():
+    __symbol = None
+    __company_name = None 
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+
+def stock_purchase_history():
+    stock1 = stock('AAPL','Apple Inc' )
+    stock2 = stock('CAT','Caterpillar' )
+    stock3 = stock('EK','Eastman Kodak' )
+    stock4 = stock('GOOG','Google' )
+    stock5 = stock('MSFT','Microsoft' )
+
+    dictionary = {stock1.get_symbol():stock1.get_company_name(), 
+                  stock2.get_symbol():stock2.get_company_name(), 
+                  stock3.get_symbol():stock3.get_company_name(), 
+                  stock4.get_symbol():stock4.get_company_name(), 
+                  stock5.get_symbol():stock5.get_company_name(), }
+
+    print("Symbol  Company name")
+    for i,j in dictionary.items(): 
+        print(i.ljust(7), j)
+    print("")
+
+
