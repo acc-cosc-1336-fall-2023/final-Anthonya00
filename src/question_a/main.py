694ad456ab22fccd470788bd36bdc0652949e51d
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-12-05 02:43:54 +0000

    question #1

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,32 @@
 #add import
+import question_a
+
+while True:
+    print("\nmenu")
+    print("option 1 - create table")
+    print("option 2 - exit menu")
+
+    option =  str(input ("Pick a number: "))
+
+    while option not in ("1" , "2"):
+        option = str(input("ERROR pick 1 or 2: "))
+
+    if option ==  "1":
+        Row = int(input("input a number for the number of rows : "))
+        colum = int(input("input a number for the number of columns :  "))
+
+        table = question_a.create_multiplication_table(Row, colum)
+        
+        question_a.display_multiplication_table(table)
+    elif option == "2":
+        print ("exiting program")
+        break
+
+    
+
+
+
+    
+
+
+
