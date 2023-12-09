e45906d7c18569c0bf6b6d46d17070f766bf5945
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2023-12-09 00:17:40 +0000

    question_d

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,22 @@
 #add import
+
+import question_d
+question_d.write_file("stocklist.txt")
+dictionary = question_d.get_stock_list("stocklist.txt")
+
+while True:
+    print ("menu")
+    print ("1 - Display purchase stock history ")
+    print("2 - exit ")
+    option = str(input("choose 1 or 2 "))
+    while option not in ('1', '2'):
+                         option = str(input("Invalid, Choose 1 or 2 "))
+    if option == '1':
+            print (" Stock purchase history ")
+            for symbol, stocks in dictionary.items():
+                    print(f"{str(stocks.get_company_name()).ljust(14)} {stocks.get_symbol()}")
+            print("")
+
+    elif option == '2':
+            print ("Exiting")
+            break
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,31 @@
 #write functions here, don't add input('') statements here!
+
+class stock():
+    __symbol = None
+    __company_name = None 
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+
+    def get_company_name(self):
+        return self.__company_name
+    
+def write_file(file):
+    enter_file = open(file,'w')
+    symbols = ['APPL', 'CAT', 'EK','GOOG','MSFT']
+    companies = ['Apple Inc', 'Caterpillar', 'Eastman Kodak', 'Google', 'Microsoft' ]
+    for i in range(len(symbols)):
+        enter_file.write(symbols[i] + " " + companies[i] + '\n')
+    enter_file.close()
+
+def get_stock_list(file_name):
+    dictionary = {}
+    in_file = open(file_name, 'r')
+    for i in in_file:
+        symbol, company_name = i.strip().split(' ', 1)
+        stocks = stock(symbol, company_name)
+        dictionary[symbol] = stocks
+    return dictionary
